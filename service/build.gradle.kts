e05714377afe7e23878fc7bda9a00f4bba7b667c
AlexeyPanchenko <aolpanchenko@gmail.com>
2024-10-27 14:31:56 +0300

    Configure targets with CI build

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,8 +1,40 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.shadowjar)
+}
+
+val executableName = "service"
+val mainPackageName = "ru.punchman.crossconf24.example"
+
+project.tasks.register<ShadowJar>("jvmBinary") {
+    group = "build"
+    manifest {
+        attributes("Main-Class" to "$mainPackageName.MainKt")
+    }
+    archiveBaseName.set(executableName)
+    archiveVersion.set("")
+    archiveClassifier.set("")
+
+    project.configure<KotlinMultiplatformExtension> {
+        targets
+            .filter { it.platformType == KotlinPlatformType.jvm && it.targetName == "jvm" }
+            .flatMap { it.compilations }
+            .filter { it.compilationName == "main" }
+            .filterIsInstance<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmCompilation>()
+            .forEach { main ->
+                from(main.output)
+                configurations = configurations + listOf(
+                    main.compileDependencyFiles as Configuration,
+                    main.runtimeDependencyFiles as Configuration,
+                )
+            }
+    }
 }
 
 kotlin {
@@ -19,6 +51,7 @@ kotlin {
                 implementation(libs.ktor.serialization.json)
                 implementation(libs.ktor.server.core)
                 implementation(libs.ktor.server.cio)
+                implementation(libs.arrow)
             }
         }
     }
@@ -27,8 +60,8 @@ kotlin {
 fun KotlinNativeTarget.configureBinaries() {
     binaries {
         executable(listOf(DEBUG, RELEASE)) {
-            entryPoint = "ru.punchman.crossconf24.example.main"
-            this.baseName = "service"
+            entryPoint = "$mainPackageName.main"
+            this.baseName = executableName
         }
     }
 }
